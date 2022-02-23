c82ecb92c537278ec967889c4cefb65357fba591
Amin Mousavi <82319487+AminMousavi223@users.noreply.github.com>
2022-02-24 00:02:43 +0330

    Single Column

--- Steel Column Design.py
+++ Steel Column Design.py
@@ -1,10 +1,48 @@
+from ast import Pass
+from re import A
+from termios import FFDLY
+import numpy as np
 
-D = 700 ; L =10  ; Pu = 0
-if Pu == 0:
-    Pu1 = 1.4*D
-    Pu2 = 1.4*D+1.6*L
-    Pu = max(Pu1, Pu2)
-    D = 0 ; L = 0
-print(float(Pu))
+# Specify the amount of loading
+def Load():
+    if Pu == 0:
+        Pu1 = 1.4*D
+        Pu2 = 1.4*D + 1.6*L
+        Pu = max(Pu1, Pu2)
+
+# Select the cross section number
+def Numbering():
+
+    return
+
+# Get the K value
+def K():
+
+    return 
+
+# Get the Fcr value
+def Fcr():
+    Landa = (k*L)/rmin
+    Fe=((np.pi**2)*E)/(Landa)**2
+    if Landa <= 139:
+        Fcr=(0.658**(Fy/Fe))*Fy
+    else:
+        Fcr=0.877*Fe
+    return
+
+
+def check():
+    Pn=Fcr*A
+    if (Pu/(0.9*Pn)) > 1:
+        Pass
+        # Return to Numbering and choose Higher one
+    elif (Pu/(0.9*Pn)) < 0.85:
+        Pass
+        # Return to Numbering and choose Lower one
+    else:
+        Pass
+        # OK
+
+
 
 

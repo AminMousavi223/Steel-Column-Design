87316ec22c904da53faee650697968e42e043eea
ali <ali.s.tag2000@gmail.com>
2022-03-13 23:20:53 -0700

    add final excel use pandas to read excel start designing single column

--- design.py
+++ design.py
@@ -1,9 +1,13 @@
 # design column object
+import pandas as pd
+
+IPB = pd.read_excel('Steel Full.xlsx','IPB' )
+IPE = pd.read_excel('Steel Full.xlsx','IPE' )
+
 
 class designColumn():
 
-
-    def __init__(self,inputValue):
+    def __init__(self, inputValue):
         ''' input value in this class is a list that is
         1)frame
         2)Pu
@@ -19,11 +23,20 @@ class designColumn():
         self.E = inputValue[3]
         self.Fy = inputValue[4]
         self.K = inputValue[5]
+        self.IPB = inputValue[6]
         try:
-            self.lengthOpening = inputValue[6]
+            self.lengthOpening = inputValue[7]
         except:
             None
 
-inputValue = ['2d',10000,4,2100000,2400,1,5]
-column = designColumn(inputValue)
-print(column)
+    def SinglColumn(self):
+        Ag = self.Pu / 1500
+        A = self.IPB.loc[3:, 'مساحت']
+        r_x = self.IPB.loc[3:, 'مساحت']
+        r_y = self.IPB.loc[3:, 'مساحت']
+        Fy = self.IPB.loc[3:, 'مساحت']
+        E = self.IPB.loc[3:, 'مساحت']
+#
+# inputValue = ['2d',10000,4,2100000,2400,1,5]
+# column = designColumn(inputValue)
+# print(column)

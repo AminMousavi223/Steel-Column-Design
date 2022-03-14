bf8730c285a9674c206f5f494cb8df892f947048
Amin Mousavi <82319487+AminMousavi223@users.noreply.github.com>
2022-03-15 00:11:12 +0330

    Design double column, (there are some bug)

--- design.py
+++ design.py
@@ -55,6 +55,38 @@ class designColumn():
 
                 return IPB.loc[i , 'IPB']
 
+    def Doublecolumn(self):
+        A1 = self.IPB.loc[3:, 'مساحت']
+        b = self.IPB.loc[3:, 'عرض بال'] 
+        r_x1 = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y1 = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور y-y']
+        I_x1 = self.IPB.loc[3:, 'ممان اینرسی حول محور x-x']
+        I_y1 = self.IPB.loc[3:, 'ممان اینرسی حول محور y-y']         
+        Fy = self.IPB.loc[3:, 'تنش تسلیم']
+        E = self.IPB.loc[3:, 'مدول الاستیسیته']
+        FS_list = []
+        A = 2*A1
+        I_x = 2*I_x1
+        I_y = 2*(I_y1 + A1*(b/2))
+        r_x = r_x1
+        r_y  = (I_y/A)**0.5
+        j = 0
+        for i in range(3, 27):
+            r_min = min(r_y[i], r_x[i])
+            landa = (self.K * self.lengthColumn) / r_min
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+
+            Pn = Fcr * A[i]
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(FS)
+            if 0.75 <= FS <= 1:
+
+                return IPB.loc[i , '2IPB']
+
 
 inputValue = ['1d', 50300, 2.7, 2100000, 2400, 1, IPB, IPE]
 column = designColumn(inputValue)

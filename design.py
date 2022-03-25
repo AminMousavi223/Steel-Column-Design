5ede8d6a39c6abe02fd41ee1ad35b9263ffdb5c7
Amin Mousavi <82319487+AminMousavi223@users.noreply.github.com>
2022-03-25 16:39:49 +0430

    Add SingleColumn_IPE, Battend_member(Not OK)

--- design.py
+++ design.py
@@ -31,7 +31,7 @@ class designColumn():
         except:
             None
 
-    def SingleColumn(self):
+    def SingleColumn_IPB(self):
         A = self.IPB.loc[3:, 'مساحت']
         r_x = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور x-x']
         r_y = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور y-y']
@@ -55,15 +55,39 @@ class designColumn():
 
                 return IPB.loc[i , 'IPB']
 
-    def Doublecolumn(self):
-        A1 = self.IPB.loc[3:, 'مساحت']
-        b = self.IPB.loc[3:, 'عرض بال'] 
-        r_x1 = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور x-x']
-        r_y1 = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور y-y'] # اضافه
-        I_x1 = self.IPB.loc[3:, 'ممان اینرسی حول محور x-x']
-        I_y1 = self.IPB.loc[3:, 'ممان اینرسی حول محور y-y']         
-        Fy = self.IPB.loc[3:, 'تنش تسلیم']
-        E = self.IPB.loc[3:, 'مدول الاستیسیته']
+    def SingleColumn_IPE(self):
+        A = self.IPE.loc[3:, 'مساحت']
+        r_x = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور y-y']
+        Fy = self.IPE.loc[3:, 'تنش تسلیم']
+        E = self.IPE.loc[3:, 'مدول الاستیسیته']
+        FS_list = []
+        j = 0
+        for i in range(3, 27):
+            r_min = min(r_y[i], r_x[i])
+            landa = (self.K * self.lengthColumn) / r_min
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+
+            Pn = Fcr * A[i]
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(FS)
+            if 0.75 <= FS <= 1:
+
+                return IPE.loc[i , 'IPE']                
+
+    def Doublecolumn_IPE(self):
+        A1 = self.IPE.loc[3:, 'مساحت']
+        b = self.IPE.loc[3:, 'عرض بال'] 
+        r_x1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور y-y'] # اضافه
+        I_x1 = self.IPE.loc[3:, 'ممان اینرسی حول محور x-x']
+        I_y1 = self.IPE.loc[3:, 'ممان اینرسی حول محور y-y']         
+        Fy = self.IPE.loc[3:, 'تنش تسلیم']
+        E = self.IPE.loc[3:, 'مدول الاستیسیته']
         FS_list = []
         j = 0
         for i in range(3, 27):
@@ -86,11 +110,50 @@ class designColumn():
             FS_list.append(FS)
             if 0.75 <= FS <= 1:
 
-                return IPB.loc[i , '2IPB']
+                return IPE.loc[i , '2IPE']
+
+    def Battend_member(self):
+        A1 = self.IPE.loc[3:, 'مساحت']
+        b = self.IPE.loc[3:, 'عرض بال'] 
+        r_x1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور y-y'] # اضافه
+        I_x1 = self.IPE.loc[3:, 'ممان اینرسی حول محور x-x']
+        I_y1 = self.IPE.loc[3:, 'ممان اینرسی حول محور y-y']         
+        Fy = self.IPE.loc[3:, 'تنش تسلیم']
+        E = self.IPE.loc[3:, 'مدول الاستیسیته']
+        FS_list = []
+        j = 0
+        for i in range(3, 27):
+            A = 2*A1[i]
+            I_x = 2*I_x1[i] # اضافه
+            I_y = 2*(I_y1[i] + A1[i]*(b[i]/2))
+            r_x = r_x1[i]
+            r_y  = (I_y[i]/A[i])**0.5
+            
+            r_min = min(r_y, r_x)
+            landa = (self.K * self.lengthColumn) / r_min
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+
+            Pn = Fcr * A
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(FS)
+            if 0.75 <= FS <= 1:
+
+                return IPE.loc[i , '2IPE']
+
+
+
+
 
 
 inputValue = ['1d', 50300, 2.7, 2100000, 2400, 1, IPB, IPE]
 column = designColumn(inputValue)
-a = column.SingleColumn()
+a = column.SingleColumn_IPB()
+b = column.SingleColumn_IPE()
+c = column.Doublecolumn_IPE()
 # print(column)
-print(a)
+print(a, b, c)

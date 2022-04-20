291144a5eaf9dead87b4dda14da0cd60ace5d3f4
Amin Mousavi <82319487+AminMousavi223@users.noreply.github.com>
2022-04-20 18:30:42 +0430

    Add Tk to design, Fix error, make the main version

--- Steel Column Design.py
+++ Steel Column Design.py
@@ -1,199 +1,232 @@
-import numpy as np
-import openpyxl as ex
-import numpy as np
+# design column object
 import pandas as pd
+import numpy as np
+from tkinter import *
+from tkinter import ttk
+from turtle import pu
 
-# input value
+IPB = pd.read_excel('Steel Full.xlsx', 'IPB')
+IPE = pd.read_excel('Steel Full.xlsx', 'IPE')
 
-def input_value():
-    # calculating pu with dead load and live load or directly
-    try:
-        D = float(input('Enter dead load (kg)'))
-        L = float(input('Enter live load (kg)'))
-        Pu = None
-    except:
-        D = None
-        L = None
+class designColumn():
 
-    if D is None or L is None:
+    def __init__(self, inputValue):
+        ''' input value in this class is a list that is
+        1)frame
+        2)Pu
+        3)length of column
+        4)E
+        5)Fy
+        6)K
+        7) (if it's exist) length of bay '''
+
+        self.frame = inputValue[0]
+        self.Pu = inputValue[1]
+        self.lengthColumn = inputValue[2]
+        self.E = inputValue[3]
+        self.Fy = inputValue[4]
+        self.K = inputValue[5]
+        self.IPB = inputValue[6]
+        self.IPE = inputValue[7]
         try:
-            Pu = float(input('Enter  critical load (kg)'))
+            self.lengthOpening = inputValue[8]
         except:
-            Pu = None
+            None
 
-    # combined loading
-    def Load(D, L, Pu):
-        if Pu == 0 or Pu == None:
-            Pu1 = 1.4 * D
-            Pu2 = 1.4 * D + 1.6 * L
-            Pu = max(Pu1, Pu2)
+    def SingleColumn_IPB(self):
+        A = self.IPB.loc[3:, 'مساحت']
+        r_x = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y = self.IPB.loc[3:, 'شعاع ژیراسیون حول محور y-y']
+        Fy = self.IPB.loc[3:3, 'تنش تسلیم']
+        E = self.IPB.loc[3:3, 'مدول الاستیسیته']
+        FS_list = []
+        j = 0
+        for i in range(3, 27):
+            r_min = min(r_y[i], r_x[i])
+            landa = (self.K * self.lengthColumn * 100) / r_min
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+            Pn = Fcr * A[i]
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(list(FS)[0])
+            if 0.75 <= list(FS)[0] <= 1:
+                return IPB.loc[i, 'IPB']
+        # if FS wasn't between 1 and 0.75 we choose nearest FS to 0.75
+        distance = []
+        for i in FS_list:
+            distance.append(abs(i - 0.75))
+        j = distance.index(min(distance)) + 3
+        return IPB.loc[j, 'IPB']
 
-        return Pu
+    def SingleColumn_IPE(self):
+        A = self.IPE.loc[3:, 'مساحت']
+        r_x = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور y-y']
+        Fy = self.IPE.loc[3:3, 'تنش تسلیم']
+        E = self.IPE.loc[3:3, 'مدول الاستیسیته']
+        FS_list = []
+        j = 0
+        for i in range(3, 27):
+            r_min = min(r_y[i], r_x[i])
+            landa = (self.K * self.lengthColumn * 100) / r_min  
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+    
+            Pn = Fcr * A[i]
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(list(FS)[0])
+            if 0.75 <= list(FS)[0] <= 1:
+                return IPE.loc[i, 'IPE']
+         # if FS wasn't between 1 and 0.75 we choose nearest FS to 0.75
+        distance = []
+        for i in FS_list:
+            distance.append(abs(i - 0.75))
+        j = distance.index(min(distance)) + 3
+        return IPE.loc[j, 'IPE']
+    
+    def Doublecolumn_IPE(self):
+        A1 = self.IPE.loc[3:, 'مساحت']
+        b = self.IPE.loc[3:, 'عرض بال']
+        r_x1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور x-x']
+        r_y1 = self.IPE.loc[3:, 'شعاع ژیراسیون حول محور y-y'] # اضافه
+        I_x1 = self.IPE.loc[3:, 'ممان اینرسی حول محور x-x']
+        I_y1 = self.IPE.loc[3:, 'ممان اینرسی حول محور y-y']
+        Fy = self.IPE.loc[3:3, 'تنش تسلیم']
+        E = self.IPE.loc[3:3, 'مدول الاستیسیته']
+        FS_list = []
+        j = 0
+        for i in range(3, 27):
+            A = 2*A1[i]
+            I_x = 2*I_x1[i] # اضافه
+            I_y = 2*(I_y1[i] + A1[i]*(b[i]/2)**2)
+            r_x = r_x1[i]
+            r_y  = (I_y/A)**0.5
+    
+            r_min = min(r_y, r_x)
+            landa = (self.K * self.lengthColumn * 100) / r_min
 
-    Pu = Load(D, L, Pu)
-    Bay = 0
+            Fe = ((np.pi ** 2) * E) / (landa ** 2)
+            if landa <= 139:
+                Fcr = (0.658 ** (Fy / Fe)) * Fy
+            else:
+                Fcr = 0.877 * Fe
+    
+            Pn = Fcr * A
+            FS = self.Pu / (0.9 * Pn)
+            FS_list.append(list(FS)[0])
+            if 0.75 <= list(FS)[0] <= 1:
+    
+                return IPE.loc[i , 'IPE']
 
-    # frame setting
-
-    frame = input('Enter what is kind of your system ? 1D, 2D or 3D ?').lower()
-    if frame in ['2d', '3d']:
-        levels = int(input('How many levels exist ?'))
-
-        if frame == '2d':
-            Bay = [int(input('how many opening exist ?'))]
-        else:
-            Bay = [int(input('How many bay exist in xy dimension '))
-                , int(input('How many bay exist in xz dimension '))]
-
-    # length calculator
-    def length_cal(frame):
-        lengthColumn = []
-        lengthOpening_xy = []
-        lengthOpening_xz = []
-        # length of column
-        if frame == '1d':
-            lengthColumn.append(float(input('enter length of column column (m) ')))
-            return [lengthColumn]
-        else:
-
-            for i in range(1, levels + 1):
-                lengthColumn.append(float(input(f'length of column level {i} (m)')))
-            # length of opening
-            for i in range(1, Bay[0] + 1):
-                lengthOpening_xy.append(float(input(f'length of Bay in xy plate {i}'
-                                                    f' (m) (start from left)')))
-            if frame == '3d':
-                for i in range(1, Bay[1] + 1):
-                    lengthOpening_xz.append(float(input(f'length of Bay in xz plate {i}'
-                                                        f' (m) (start from left)')))
-                return [lengthColumn, lengthOpening_xy, lengthOpening_xz]
-
-            return [lengthColumn, lengthOpening_xy]
-
-    length = length_cal(frame)
-    lengthColumn = length[0]
-    if frame == '3d':
-        lengthOpening = [length[1], length[2]]
-    elif frame == '2d':
-        lengthOpening = [length[1]]
-
-
-
-
-
-    E = float(input('Enter E of your steel '))
-    Fy = float(input('Enter Fy of your steel '))
-
-    # K parameter or prerequisites
-    try:
-        k = float(input('Enter K if this parameter is clear ,otherwise just press enter'))
-        if frame != '1d':
-            return [frame, Pu, lengthColumn, E, Fy, lengthOpening , k]
-        else:
-            return [frame, Pu, lengthColumn, E, Fy, k]
-
+# Start GUI by Tkinter
+def input_output():
+    global IPB, IPE
+    # calculating pu with dead load and live load or directly
+    try:  
+        Pu = float(pu.get())
     except:
-        if frame == '1d':
+        D = float(d.get())
+        Li = float(li.get())
+        Pu1 = 1.4 * D
+        Pu2 = 1.4 * D + 1.6 * Li
+        Pu = max(Pu1, Pu2)
+  
+    inputValue = ['1d', Pu, float(L.get()), int(E.get()) ,int(Fy.get()), float(k.get()), IPB, IPE]
+    column = designColumn(inputValue)
+    Lis.insert(END, f'Best performance for IPB sections: IPB{int(column.SingleColumn_IPB())}' )
+    Lis.insert(END, f'Best performance for IPE sections: IPE{int(column.SingleColumn_IPE())}' )
+    Lis.insert(END, f'Best performance for 2IPE sections: 2IPE{int(column.Doublecolumn_IPE())}' )
+    
+def cls():
+    Lis.delete(0, END)
+    # plt.cla()
 
-            supportDown = input("what's your support in down of column ?")
-            supportUp = input("what's your support in top of column ?")
-            return [frame, Pu, lengthColumn, E, Fy, supportUp , supportDown]
+# Window settings
+W = Tk()
+W.title("Steel Column Design")
+W.geometry('550x400')
 
+# Scrollbar
+Scr = Scrollbar(W)
+Scr.pack(side=RIGHT, fill=BOTH)
+frame1 = Frame(W)
+frame1.pack(expand=True, fill=BOTH)
 
-        else:
-            try:
-                Ga = float(input('Enter Ga'))
-                Gb = float(input('Enter Gb'))
+# Menu 
+menu = Menu(W)
+M1 = Menu(menu, tearoff=0)
+M1.add_command(label='New File')
+M1.add_command(label='Open File...')
+M1.add_command(label='Save')
+M1.add_command(label='Save As...')
+M1.add_command(label='Close', command=W.quit)
+M2 = Menu(menu, tearoff=0)
+M2.add_command(label='Undo')
+M2.add_command(label='Redo')
+M2.add_command(label='Cut')
+M2.add_command(label='Copy')
+M2.add_command(label='Pase')
+M3 = Menu(menu, tearoff=0)
+M3.add_command(label='Slove')
+M3.add_command(label='Solution...')
+M4 = Menu(menu, tearoff=0)
+menu.add_cascade(label='File', menu=M1)
+menu.add_cascade(label='Edit', menu=M2)
+menu.add_cascade(label='Solver', menu=M3)
+menu.add_cascade(label='Help', menu=M4)
+W.config(menu=menu)
 
+# Labe Frame - entry
+frame2 = Frame(frame1)
+Lo = LabelFrame(frame2, text='Loading')
+Label(Lo, text='Pu').pack()
+pu = Entry(Lo)
+pu.pack()
+Label(Lo, text='Dead Load').pack()
+d = Entry(Lo)
+d.pack()
+Label(Lo, text='Live Load').pack()
+li = Entry(Lo)
+li.pack()
 
-            except:
-                    columnNum_a = int(input('How many column is connect to point a'))
-                    beamNum_a = int(input('How many beam is connect to point a'))
-                    columnNum_b = int(input('How many column is connect to point b'))
-                    beamNum_b = int(input('How many beam is connect to point b'))
-                    Sigma = 0
-                    sigma = []
-                    for i in [columnNum_a , beamNum_a , columnNum_b , beamNum_b]:
-                        for j in range(1,i+1) :
-                            E = float(input(f'Enter E value of your {j} column or beam'))
-                            I = float(input(f'Enter inertia moment of your {j} column or beam'))
-                            L_joint = float(input(f'Enter lentgh of your {j} column or beam'))
-                            Sigma += (E*I)/L_joint
-                            sigma.append(Sigma)
-                    Ga = sigma[0] / sigma[1]
-                    Gb = sigma[2] / sigma[3]
-            return [frame, Pu, lengthColumn, E, Fy, lengthOpening, Ga, Gb]
+C_S = LabelFrame(frame2, text='Column Specifications')
+Label(C_S, text='L - Length').pack()
+L = Entry(C_S)
+L.pack()
+Label(C_S, text='E - Modulus of Elasticity').pack()
+E = Entry(C_S)
+E.pack()
+Label(C_S, text='\u03C3 y - Yield Strength').pack()
+Fy = Entry(C_S)
+Fy.pack()
+Label(C_S, text='K').pack()
+k = Entry(C_S)
+k.pack()
 
-    return [frame, Pu, lengthColumn, E, Fy, lengthOpening]
+#Combo Box
+CH = LabelFrame(frame2, text='Section Selection')
+val = ['IPE' , 'IPB' , 'Bosted IPE' , 'Bosted IPB' , '2IPE' , 'HSS' , 'HSS']
+Co = ttk.Combobox(CH,values=val)
+Co.set('Section')
+Co.pack()
 
-input_value()
+Bu = LabelFrame(frame2, text='')
+but1 = Button(Bu, text='Compute', command=input_output).pack(pady=20,ipadx=40)
+but2 = Button(Bu, text='Clear', command=cls).pack(pady=20,ipadx=40)
+Lis = Listbox(frame2, width=35, height=5)
 
+# Pack-grid
+frame2.grid(row=0, column=0)
+Lo.grid(row=0, column=0, padx=50, pady=20)
+C_S.grid(row=1, column=0, pady=20)
+Bu.grid(row=1, column=1, pady=20,ipadx=40)
+Lis.grid(row=0, column=1, padx=10, pady=10, ipadx=20, ipady=20)
 
-
-i = 5
-def Numbering():
-    num = 1
-    while num != 0:
-        def Excel():
-            IPE = ex.load_workbook('Steel Full IPE.xlsx')
-            IPE = IPE.active
-            Number = IPE.cell(row=i, column=1).value
-            A = IPE.cell(row=i, column=10).value
-            r_x = IPE.cell(row=i, column=15).value
-            r_y = IPE.cell(row=i, column=19).value
-            Fy = IPE.cell(row=i, column=22).value
-            E = IPE.cell(row=i, column=24).value
-            return Number, A, r_x, r_y, Fy, E
-
-        # # Get the K value
-        # def K():
-        #     return 
-
-        # Get the Fcr value
-        def Get_Fcr():
-            Excel_Data = Excel()
-            A = float(Excel_Data[1])
-            r_x = float(Excel_Data[2])
-            r_y = float(Excel_Data[3])
-            Fy = float(Excel_Data[4])
-            E = float(Excel_Data[5])
-            k_x= 1; k_y= 1; L= 5    # Should be delete!
-            Landa_x = (k_x*L)/r_x
-            Landa_y = (k_y*L)/r_y
-            Landa = max(Landa_x,Landa_y)
-            Fe=float((np.pi**2)*E)/((Landa)**2)   
-            if Landa <= 139:
-                fcr=(0.658**(Fy/Fe))*Fy
-            else:
-                fcr=0.877*Fe
-            return fcr
-
-        def check():
-            Fcr_Data = Get_Fcr()
-            Fcr = Fcr_Data[0]
-            Excel_Data = Excel()
-            A = Excel_Data[1]
-            Pn=Fcr*A
-            Pu = 10000    # Should be delete!
-            if (Pu/(0.9*Pn)) > 1:   
-                Conditions =  "Next IPE"
-                # Return to Numbering and choose Higher one
-                num = 1
-            elif (Pu/(0.9*Pn)) < 0.85:
-                Conditions = "Nothig"     
-                # Return to Numbering and choose Lower one
-                num = 1
-            else:
-                Conditions = "OK"
-                num = 0
-                # break
-            return num, Conditions
-        chek_Data = check()
-        num = chek_Data[0]
-
-    return 
-
-
-
-
-
+# Run Tkinter
+W.mainloop()
